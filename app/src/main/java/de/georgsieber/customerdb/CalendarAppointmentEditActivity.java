de7fb2d3a5a5816533f66780aad90198cdee3902
Georg Sieber <it@georg-sieber.de>
2020-06-28 18:00:48 +0200

    add QR code feature

--- app/src/main/java/de/georgsieber/customerdb/CalendarAppointmentEditActivity.java
+++ app/src/main/java/de/georgsieber/customerdb/CalendarAppointmentEditActivity.java
@@ -1,9 +1,13 @@
 package de.georgsieber.customerdb;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,19 +18,26 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.NumberPicker;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.text.DateFormat;
 import java.text.ParseException;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.georgsieber.customerdb.model.CustomerAppointment;
 import de.georgsieber.customerdb.model.CustomerCalendar;
@@ -49,9 +60,11 @@ public class CalendarAppointmentEditActivity extends AppCompatActivity {
     EditText mEditTextTitle;
     EditText mEditTextCustomer;
     EditText mEditTextNotes;
+    EditText mEditTextLocation;
     Button mButtonDay;
     TimePicker mTimePickerStart;
     TimePicker mTimePickerEnd;
+    ImageView mImageViewQrCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,15 +88,26 @@ public class CalendarAppointmentEditActivity extends AppCompatActivity {
         ColorControl.updateActionBarColor(this, mSettings);
 
         // find views
+        mImageViewQrCode = findViewById(R.id.imageViewQrCode);
         mSpinnerCalendar = findViewById(R.id.spinnerCalendar);
         mEditTextTitle = findViewById(R.id.editTextTitle);
         mEditTextNotes = findViewById(R.id.editTextNotes);
         mEditTextCustomer = findViewById(R.id.editTextCustomer);
+        mEditTextLocation = findViewById(R.id.editTextLocation);
         mButtonDay = findViewById(R.id.buttonDay);
         mTimePickerStart = findViewById(R.id.timePickerStart);
         mTimePickerStart.setIs24HourView(true);
         mTimePickerEnd = findViewById(R.id.timePickerEnd);
         mTimePickerEnd.setIs24HourView(true);
+
+        // register events
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                refreshQrCode();
+            }
+        }, 1000, 1000);
 
         // load calendars
         mCustomerCalendars = mDb.getCalendars(false);
@@ -144,6 +168,52 @@ public class CalendarAppointmentEditActivity extends AppCompatActivity {
         }
     }
 
+    private String mLastQrContent = "";
+    @SuppressWarnings("SuspiciousNameCombination")
+    private void refreshQrCode() {
+        int WIDTH = 1200;
+        if(!updateAppointmentObjectByInputs()) return;
+        @SuppressLint("SimpleDateFormat") DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+        String content = "BEGIN:VEVENT" + "\n"
+                + "SUMMARY:" + mEditTextTitle.getText().toString() + "\n"
+                + "DESCRIPTION:" + mEditTextNotes.getText().toString().replace("\n", "\\n") + "\n"
+                + "LOCATION:" + mEditTextLocation.getText().toString() + "\n"
+                + "DTSTART:" + dateFormat.format(mCurrentAppointment.mTimeStart) + "\n"
+                + "DTEND:" + dateFormat.format(mCurrentAppointment.mTimeEnd) + "\n"
+                + "END:VEVENT" + "\n";
+        if(mLastQrContent.equals(content)) return;
+        mLastQrContent = content;
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, WIDTH, WIDTH);
+            int w = bitMatrix.getWidth();
+            int h = bitMatrix.getHeight();
+            int[] pixels = new int[w * h];
+            for (int y = 0; y < h; y++) {
+                int offset = y * w;
+                for (int x = 0; x < w; x++) {
+                    pixels[offset + x] = bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE;
+                }
+            }
+            final Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, WIDTH, 0, 0, w, h);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mImageViewQrCode.setImageDrawable(new BitmapDrawable(bitmap));
+                }
+            });
+
+        } catch (WriterException ignored) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mImageViewQrCode.setImageDrawable(new ColorDrawable(Color.WHITE));
+                }
+            });
+        }
+    }
+
     private void fillFields(CustomerAppointment a) {
         for(CustomerCalendar c : mCustomerCalendars) {
             if(c.mId == a.mCalendarId) {
@@ -173,12 +243,11 @@ public class CalendarAppointmentEditActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     private boolean updateAppointmentObjectByInputs() {
-        CustomerCalendar calendar = ((CustomerCalendar) ((Spinner) mSpinnerCalendar).getSelectedItem());
-        if(calendar == null) {
-            CommonDialog.show(this, getString(R.string.error), getString(R.string.no_calendar_selected), CommonDialog.TYPE.WARN, false);
-            return false;
-        }
+        CustomerCalendar calendar = (CustomerCalendar) (mSpinnerCalendar).getSelectedItem();
+        if(calendar == null) return false;
+
         mCurrentAppointment.mCalendarId = calendar.mId;
         mCurrentAppointment.mTitle = mEditTextTitle.getText().toString();
         mCurrentAppointment.mNotes = mEditTextNotes.getText().toString();
@@ -201,6 +270,22 @@ public class CalendarAppointmentEditActivity extends AppCompatActivity {
             return false;
         }
 
+        // save default length
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putInt("appointment-length", (int)((mCurrentAppointment.mTimeEnd.getTime() - mCurrentAppointment.mTimeStart.getTime()) / 1000 / 60));
+        editor.apply();
+
+        return true;
+    }
+    private boolean updateAndCheckAppointment() {
+        CustomerCalendar calendar = (CustomerCalendar) (mSpinnerCalendar).getSelectedItem();
+        if(calendar == null) {
+            CommonDialog.show(this, getString(R.string.error), getString(R.string.no_calendar_selected), CommonDialog.TYPE.WARN, false);
+            return false;
+        }
+
+        if(!updateAppointmentObjectByInputs()) return false;
+
         if(mCurrentAppointment.mTimeStart.getTime() > mCurrentAppointment.mTimeEnd.getTime()) {
             CommonDialog.show(this, getString(R.string.error), getString(R.string.end_date_before_start_date), CommonDialog.TYPE.WARN, false);
             return false;
@@ -210,16 +295,11 @@ public class CalendarAppointmentEditActivity extends AppCompatActivity {
             return false;
         }
 
-        // save default length
-        SharedPreferences.Editor editor = mSettings.edit();
-        editor.putInt("appointment-length", (int)((mCurrentAppointment.mTimeEnd.getTime() - mCurrentAppointment.mTimeStart.getTime()) / 1000 / 60));
-        editor.apply();
-
         return true;
     }
 
     private void saveAndExit() {
-        if(updateAppointmentObjectByInputs()) {
+        if(updateAndCheckAppointment()) {
             if(mCurrentAppointmentId == -1) {
                 // insert new
                 mDb.addAppointment(mCurrentAppointment);
